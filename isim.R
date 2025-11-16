d9295d1feaf4c506ccbc0503df3d6dec222ae399
Sarah Bolinger <sarah.bolinger@gmail.com>
2025-11-16 13:46:24 -0600

    initial commit. make one script with more cmd line arguments

---
+++ isim.R
@@ -0,0 +1,173 @@
+
+  # col_sel <- c(prVars,r) # columns to select, as strings
+# source("fateGLM_impsim.R")
+library(brglm2) # penalized logistic regression
+library(CALIBERrfimpute) # could access using ::
+library(gt)
+library(gtsummary)
+library(tidyverse)
+source("mice_functions.R")
+source("imp_sim_functions.R")
+source("missing_data.R")
+source("other_imp.R")
+
+#########################################################################################
+
+params <- list(nrun=100, hdir="/home/wodehouse/projects/fate_glm/",
+               deb=FALSE,
+               xdeb=FALSE, # for obsessively checking things - not useful for normal debugging & lots of text output
+               ipl=FALSE,
+               lin=FALSE,
+               seed=61389,
+               m=20)
+
+arg <- commandArgs(trailingOnly=TRUE)
+if(length(arg)==0){
+  # stop("at minimum, needs argument for 'lin' (T or F)")
+  cat("\n\n/////////////////////////////////////////////////////////////////////////////////////////////\n")
+  cat("\n** NOTE ** no arguments provided - using default of linux = FALSE\n\n")
+  cat("/////////////////////////////////////////////////////////////////////////////////////////////\n\n\n")
+} else if(length(arg) > 0){
+  cat("arg =")
+  print(arg)
+  for(a in arg){
+    # if(is.numeric(a)) params$nrun <- a
+    if(grepl("^\\d+$", a)) params$nrun  <- a
+    else if(a=="lin") params$lin        <- TRUE
+    else if(a=="deb") params$deb        <- TRUE
+    else if(a=="xdeb") params$xdeb        <- TRUE
+    else if(a=="ipl") params$ipl       <- TRUE
+    else if(a=="is_u" | a == "HF_mis") params$resp       <- a
+    else if(grepl("s\\d+", a)) params$seed <- as.numeric(str_extract(a, "\\d+"))
+    else if(grepl("m\\d+", a)) params$m <- as.numeric(str_extract(a, "\\d+"))
+    # else if()
+  }
+}
+#########################################################################################
+
+debugging <- FALSE # for uickly setting values when working in the file with the functions
+suffix <- sprintf("%sruns", params$nrun)
+cat(">> home directory:", params$hdir, "\t> & number of runs:", params$nrun, "\t> & output suffix:", suffix)
+
+modList <- readLines("modList.txt")
+mods4sim <- modList[c(1,8,16) ]
+names(mods4sim) <- c("m1", "m8", "m16")
+formulas <- readRDS("form_lists.rds")
+metLists <- readRDS("met_lists.rds")
+
+dat4sim <- read.csv("dat_complete.csv", stringsAsFactors = TRUE)
+dat4sim$cam_fate <- relevel(dat4sim$cam_fate, ref="H") # make 'H' the reference category
+dat4sim$species <- relevel(dat4sim$species, ref="LETE")
+levels(dat4sim$HF_mis) <- c(0,1)
+levels(dat4sim$is_u)   <- c(0,1)
+ 
+prVars <- c("species", "cam_fate", "obs_int", "nest_age", "fdate")
+vars <-  c("nest_age", "cam_fateD", "cam_fateA", "cam_fateF", "cam_fateHu", "cam_fateS", "speciesCONI", "speciesCONI:nest_age", "speciesCONI:obs_int", "obs_int", "fdate") # all vars
+mets <- c("default","pmm", "rf", "cart", "caliber","passive", "stratify","cc")# don't need full here?
+
+resp <- "is_u"
+col_list<- c(prVars,resp )# columns to select, as strings
+form_list <- formulas[[resp]]
+# seeds <- c( 11153, 71358)
+
+#########################################################################################
+
+cat("\n\n>> number of imputations:", params$m, class(params$m))
+cat("\t>> & methods to test:", mets)
+# cat("\n\n>> bias to be calculated:", bias_names, "\n")
+cat("\n\n>>>> date & time:", format(Sys.time(), "%d-%b %H:%M\n"))
+
+cat("\n\n********************************************************************************************")
+cat("\n>> response:", resp,"\n\t& columns for imputation:", col_list)
+cat("\n********************************************************************************************\n\n")
+
+res <- array(NA, dim = c(length(vars), length(mets), params$nrun, 3, length(mods4sim)))
+# dimnames(res) <- list(c("pmm", "rf"),
+dimnames(res) <- list(sort(as.character(vars)),
+                      # c("pmm", "rf", "cart"),
+                      as.character(mets),
+                      as.character(1:params$nrun),
+                      # c("estimate", "2.5 %","97.5 %","fmi"),
+                      c("estimate", "2.5 %","97.5 %"),
+                      names(mods4sim)
+                      )
+
+cat(sprintf(">>> running simulation %s times. seed = %s\n\n", params$nrun, params$seed))
+
+# for (seed in seeds){
+  # cat("seed=",params$seed, " - ")
+for(run in 1:params$nrun){
+  cat(run)
+  datNA <- mkSimDat(nd = dat4sim, seeed = run+params$seed, vars=vars, method = "amp", wt = TRUE, xdebug=params$xdeb, debug = params$deb, convFact = TRUE)
+  datNA <- datNA$amp
+  
+  for(x in seq_along(mets)){ # does matching by index help the trycatch statement?
+    ## *~*~*~*~*
+    # if (xdebug) cat("\n\n>>>> method:", x)
+    skiptoNext <- FALSE
+    
+    tryCatch(
+      expr = {
+        vals <- mkImpSim(ampDat=datNA,
+                         cols=col_list,
+                         resp=resp, 
+                         form_list =form_list,
+                         vars=vars,
+                         mods=mods4sim,
+                         met=mets[x],
+                         debug = params$debug,
+                         m=params$m, 
+                         xdebug=params$xdebug,
+                         impplot=params$ipl
+                         )
+        # imp <- eval(parse(text=impCall))
+        # skiptoNext <- FALSE
+        # if(length(imp$loggedEvents > 0)) print(imp$loggedEvents)
+      },
+      error = function(e){
+        cat("\nERROR:", conditionMessage(e), "\n")
+        # next
+        # return(NULL)
+        # skiptoNext <- TRUE
+        skiptoNext <<- TRUE  # superassignment operator- not sure if necessary
+        # imp <- list(imp=NA)
+        # ret[,,y]
+        # continue()
+      }
+    )
+    if(skiptoNext) next
+    
+    vmatch <- match(rownames(vals), rownames(res)) # col 1 of vals is the row names
+    res[vmatch, mets[x], run,,]  <- vals
+  }
+  
+  j <- 50
+  if(run %% j == 0){
+  # if(run %% 4 == 0){
+    begn <- run-j
+    endd <- run-0
+    nowtime <- format(Sys.time(), "%d%b%H%M")
+    fname <- paste(sprintf("out/runs%sto%s_%s.rds", begn, endd, nowtime))
+    saveRDS(res[,,begn:endd,,], fname)
+    cat(sprintf(">>>>>> saved runs %s to %s to file!", begn, endd))
+  }
+  # return(res)
+}
+# }
+
+
+# }
+# imp_sim <- runSim(fullDat=dat4sim,col_sel = col_list,mets = met_list,forms=form_list, resp = r, vars = var_list, mods = mods4sim,par=params) # don't want to set seed
+# if(debug) Rprof()
+# imp_sim_m <- runSim(fullDat=dat4sim,col_sel = col_list,mLists = metLists,forms=form_list, resp = r, vars = var_list, mods = mods4sim,par=params) # don't want to set seed
+# if(debug) Rprof(NULL)
+# if(params$deb){
+#   cat("\n\n********************************************************************************************\n")
+#   cat(">>>>> BIAS VALUES: \n")
+#   cat("********************************************************************************************\n")
+# }
+# # bias_out <- parAvg(fullDat = dat4sim, impDat = imp_sim,resp = r, vars = var_list, mod = mods4sim[z], mets = met_list, biasVals = bias_names, debug = params$deb)
+# # bias_out <- parAvg(fullDat = dat4sim, impDat = imp_sim,resp = r, vars = var_list, modnum = z, mets = met_list, biasVals = bias_names, debug = params$deb)
+# bias_out <- parAvg(fullDat = dat4sim, impDat = imp_sim,hdir = params$hdir,resp = r, vars = var_list, mods=mods4sim, mets = met_list, biasVals = bias_names, debug = params$deb, xdebug=params$xdeb)
+# }
+# }
