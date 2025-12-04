ed9714f0e31687782d1686cedf9e77516a8e8556
Sarah <sarah.bolinger@gmail.com>
2025-12-04 15:08:02 -0600

    source separate function file with print debugging for tests

--- isim.R
+++ isim.R
@@ -11,8 +11,9 @@ library(data.table)
 suppressMessages(library(mice))
 suppressMessages(library(tidyverse))
 # look into tidytable for tidyverse syntax w/ data.table
-s_files <- c("mice_functions.R", "missing_data.R", "other_imp.R")
-lapply(s_files, source)
+options(width=90)
+#if(params$deb|params$xdeb) func = "debug_imp_sim_func.R" else func = "imp_sim_functions.R"
+#source(func)
 #source("mice_functions.R")
 #source("imp_sim_functions.R")
 #source("missing_data.R")
@@ -22,10 +23,11 @@ lapply(s_files, source)
 #########################################################################################
 
 params <- list(nrun=100,
-#params <- c(nrun=100,
 	       hdir="/home/wodehouse/Projects/fate_glm/",
 	       #outdir = paste0(format(Sys.time(), "%d%b"),"/"),
+               suffix = "",
 	       outdir = format(Sys.time(), "%d%b"),
+               ampWt=1,
                deb=FALSE,
                xdeb=FALSE, # for obsessively checking things - not useful for normal debugging & lots of text output
                ipl=FALSE,
@@ -43,9 +45,9 @@ if(length(arg)==0){
 	cat("** NOTE ** no arguments provided - using default of windows = FALSE\n")
 	cat("/////////////////////////////////////////////////////////////////////////////////////////////\n\n")
 } else if(length(arg) > 0){
-  cat("\n/////////////////")
+  cat("\n\n/////////////////////")
   cat("  arg =  ", paste(unlist(arg), collapse=" ; "))
-  cat("  /////////////////\n")
+  cat("  ////////////////////////////////\n")
   for(a in arg){
 	  if(grepl("r\\d+$", a)) params$nrun  <- as.numeric(str_extract(a, "\\d+"))
 	  else if(a=="win") params$win        <- TRUE
@@ -54,11 +56,24 @@ if(length(arg)==0){
 	  else if(a=="ipl") params$ipl       <- TRUE
 	  # else if(a=="is_u" | a == "HF_mis") params$resp       <- a
           else if(a=="test") params$test <- TRUE
+	  else if(grepl("suff_\\w+", a)) params$suffix <- as.numeric(str_extract(a, "(?<=suff_)\\w+"))
+	  else if(grepl("aw\\d", a)) params$ampWt <- as.numeric(str_extract(a, "\\d"))
 	  else if(grepl("j\\d+", a)) params$j <- as.numeric(str_extract(a, "\\d+"))
 	  else if(grepl("s\\d+", a)) params$seeds <- c(as.numeric(str_extract(a, "\\d+")))
 	  else if(grepl("m\\d+", a)) params$m <- as.numeric(str_extract(a, "\\d+"))
   }
 }
+
+#########################################################################################
+
+s_files <- c("mice_functions.R", "missing_data.R", "other_imp.R", "imp_sim_functions.R")
+if(params$deb|params$xdeb) s_files[4] = "debug_imp_sim_func.R" 
+cat("\n<><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>\n")
+#cat("\n###################################################################")
+cat("\n>>> sourcing files:", paste(s_files,collapse=" ; "))
+#lapply(s_files, function(x) invisible(source(x)))
+invisible(lapply(s_files, function(x) source(x)))
+
 #########################################################################################
 
 bprint <- function(x) print(rbind(head(x, 3), tail(x,3)))
@@ -69,55 +84,12 @@ if(params$win) cat("\n[][][][][][][][][][][][][][][][][][][][][][][][][][][][][]
 if(params$win) params$hdir <- "C:/Users/sarah/Dropbox/Models/fate_glm/"
 if(params$j > params$nrun) stop("j must be less than or equal to nrun!")
 # if(is.null(params$resp)) stop("no response variable specified!")
-
-modList <- readLines("modList.txt")
-mods4sim <- modList[c(1,8,16) ]
-names(mods4sim) <- c("m1", "m8", "m16")
-
-#### for the imputation: ####################################################################
-formulas <- readRDS("form_lists.rds")
-metLists <- readRDS("met_lists.rds")
-#cat("\n>> methods for individual variables:\n")
-#print(metLists)
-
-#### for creating the simulated data: #######################################################
-betas <- readRDS("betas.rds") # this is a list of vectors or something
-mList <- readRDS('means.rds')
-cMat  <- readRDS('cormat.rds')
-fprob <- c( 'H'=0.53,'A'=0.1, 'D'=0.13,'F'=0.06, 'Hu'=0.13,'S'=0.06 )
-sprob <- c('CONI'=0.32, 'LETE'=0.68)
-nnest <- ifelse(params$test==T, 100, 150) # number of simulated nests
-mpatt  <- readRDS("misPatt.rds")
-ampwt <- readRDS("ampWts.rds")
-if(params$deb) cat("\n>> Missingness pattern & variable weights:")
-if(params$deb) print(mpatt)
-if(params$deb) print(ampwt)
-
-cat("sort vars or not???")
-vars <-  c("nest_age", "cam_fateD", "cam_fateA", "cam_fateF", "cam_fateHu", "cam_fateS", "speciesCONI", "speciesCONI:nest_age", "speciesCONI:obs_int", "obs_int", "fdate") # all vars
-prVars <- c("species", "cam_fate", "obs_int", "nest_age", "fdate")
-mets <- c("default", "cart", "caliber","passive", "stratify","cf_cc","cc")# don't need full here?
-resp_list <- c("is_u", "HF_mis")
-
 if(params$test){
-  mets <- c("cc", "cart", "caliber", "passive")
-  #params[c("j", "nrun", "m", "deb")] <- c(1, 3, 5, TRUE)
-  #cat("\t*** USING TEST VALUES ***\t", params[[c("j", "nrun", "m", "deb")]], " and mets:", mets,"\n")
-  #cat("\t*** USING TEST VALUES ***\t", lapply(params, `[[`, c("j", "nrun", "m", "deb")), mets,"\n")
-  #cat("\t*** USING TEST VALUES ***\t", lapply(params, print), mets,"\n")
-  #cat("\t*** USING TEST VALUES ***\t", paste(c(params$j, params$m, params$nrun), collapse=";"),"\n\n")
-  #cat("and methods:",mets,"\n")
-  #params$j <- 1
-  #params$seeds <- 713
-  #params$nrun <- 3
-  #params$m <- 5
-  #params$deb <- TRUE
+    #mets <- c("cc", "cart", "caliber", "passive")
+    params$outdir <- paste("t",params$outdir,sep="-")
 }
 
-if(params$deb|params$xdeb) func = "debug_imp_sim_func.R" else func = "imp_sim_functions.R"
-source(func)
-
-suffix <- sprintf("%sruns", params$nrun)
+suffix <- paste(sprintf("%sruns", params$nrun), params$suffix, sep="_")
 now_dir <- paste(params$hdir, params$outdir, sep="out/")
 if(!dir.exists(now_dir)) dir.create(now_dir)
 
@@ -137,18 +109,57 @@ if(is.null(params$seeds)){
 	cat("\n>>> using default seed list, in new order:", params$seeds, "\t")
 }
 
+
+modList <- readLines("modList.txt")
+mods4sim <- modList[c(1,8,16) ]
+names(mods4sim) <- c("m1", "m8", "m16")
+
+#### for the imputation: ####################################################################
+formulas <- readRDS("form_lists.rds")
+metLists <- readRDS("met_lists.rds")
+#cat("\n>> methods for individual variables:\n")
+#print(metLists)
+
+#### for creating the simulated data: #######################################################
+betas <- readRDS("betas.rds") # this is a list of vectors or something
+mList <- readRDS('means.rds')
+cMat  <- readRDS('cormat.rds')
+fprob <- c( 'H'=0.53,'A'=0.1, 'D'=0.13,'F'=0.06, 'Hu'=0.13,'S'=0.06 )
+sprob <- c('CONI'=0.32, 'LETE'=0.68)
+nnest <- ifelse(params$test==T, 200, 150) # number of simulated nests
+mpatt  <- readRDS("misPatt.rds")
+awFile <- case_when(params$ampWt==1 ~ "ampWts.rds",
+                    params$ampWt==2 ~ "ampWts2.rds",
+                    params$ampWt==3 ~ "ampWts3.rds")
+ampwt <- readRDS(awFile)
+cat("\n<><><><><><><><><><> Missingness pattern & variable weights: <><><><><><><><><><><><><><><><><><><><>\n")
+print(mpatt)
+print(ampwt)
+
+#########################################################################################
+
+#cat("sort vars or not???")
+vars <-  c("nest_age", "cam_fateD", "cam_fateA", "cam_fateF", "cam_fateHu", "cam_fateS", "speciesCONI", "speciesCONI:nest_age", "speciesCONI:obs_int", "obs_int", "fdate") # all vars
+prVars <- c("species", "cam_fate", "obs_int", "nest_age", "fdate")
+mets <- c("default", "cart", "caliber","passive", "stratify","cf_cc","cc")# don't need full here?
+resp_list <- c("is_u", "HF_mis")
+
 #form_list <- formulas[[params$resp]]
 #########################################################################################
 
-cat("\n\n>> methods:", mets)
-cat(sprintf("\n>> total reps: %s x %s", length(params$seeds), params$nrun)) 
-if(params$deb) cat("\t- DEBUG ON")
-if(params$xdeb) cat("EXTRA")
+cat("\n<><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>\n")
+#cat("\n<><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>\n")
+cat("\n>> METHODS:", mets)
+cat(sprintf("\t>> TOTAL REPS: %s x %s WITH %s NESTS EACH", length(params$seeds), params$nrun, nnest)) 
+if(params$deb) cat("\t- debug ON")
+if(params$xdeb) cat(" + EXTRA")
 #cat("\t>> response:", params$resp,"\n\t& cols for imputation:", col_list)
-cat("\n\n****************************************************************************")
+#cat("\n\n****************************************************************************")
 cat("\n>> output will be saved every", params$j, "runs to dir:", now_dir,"\n\n")
 
 for (seed in params$seeds){
+    outf <- paste0(seed, "_loggedEvents.out")
+    if(params$test) outf <- paste0(seed,"_loggedEvents_TEST.out")
     res <- array(NA, dim = c(length(vars), length(mets), params$nrun, 3, length(mods4sim), length(resp_list)))
     # dimnames(res) <- list(sort(as.character(vars)),
     dimnames(res) <- list(as.character(vars),# need to be in same order as vals
@@ -159,44 +170,42 @@ for (seed in params$seeds){
                           resp_list
                           )
 
-    cat(sprintf(">>> running simulation %s times \t>>> seed = %s ", params$nrun, seed))
+#cat("\n<><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>\n")
+    cat(sprintf(">>>>>>>>>>>>>> running simulation %s times \t>>> seed = %s ", params$nrun, seed))
     cat("\t>> & no. imp.:", params$m, "\n\n")
     for(mod in seq_along(mods4sim)){
         # select the correct list of formulas and list of beta values!
-        cat("\n()()()()()() MODEL", mods4sim[mod], " ()()()()()()()()()()()()()()()() \n") ## *~*~*~*~*
+        #cat("\n()()()()()() MODEL", mods4sim[mod], " ()()()()()()()()()()()()()()()() \n\n") ## *~*~*~*~*
+        cat("\n\n::::::::::::::::::: MODEL", mods4sim[mod], ":::::::::::::::::::::::::::::::\n\n") ## *~*~*~*~*
         beta_list <- betas[[mod]]
         form_list <- formulas[[names(mods4sim)[mod]]]
         #cat("\n>> formula list, beta list:") ## *~*~*~*~*
         #print(names(form_list))
         #print(str(beta_list))
         for(run in 1:params$nrun){
-                cat(run)
+            cat(run)
             # repeat this until you get a dataset w/o missing levels??
-            dat4sim <- mkSim(resp_list, mods4sim[mod], nnest, cMat, mList, beta_list, fprob, sprob, prList, debug=params$deb)
+            #dat4sim <- mkSim(resp_list, mods4sim[mod], nnest, cMat, mList, beta_list, fprob, sprob, prList, debug=params$deb)
+            dat4sim <- mkResp(resp_list, mods4sim[mod], nnest, cMat, mList, beta_list, fprob, sprob, prList, debug=params$deb)
             datNA <- mkSimDat( seeed = run+seed, nd = dat4sim, mpatt=mpatt, wts=ampwt, xdebug=params$xdeb, debug = params$deb, convFact = TRUE)
             # if(params$deb) cat("\n*** datNA:\n")
-            # if(params$deb) print(datNA)
+            #if(params$deb) cat("\n<><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><><>\n")
+            if(params$deb) cat("\n\n<><><><><><><><><><<><><><><<><><><><> AMPUTED DATA: <><><><><><><><><><<><><><><<><><><><>>> ")
+            if(params$deb) qvcalc::indentPrint(str(datNA$amp), indent=8)
+            if(params$deb) qvcalc::indentPrint(colSums(is.na(datNA$amp)), indent=8)
             datNA <- datNA$amp
             
-            # prv<- profvis::profvis(
-            if(FALSE){
-              mets <- c("cart", "cc", "default")
-            }
             for(x in seq_along(mets)){ # does matching by index help the trycatch statement?
-              ## *~*~*~*~*
-              # if (params$deb) cat("\n\n<><><><><><><><> method:", mets[x], "\t- ")
-              #if (params$deb) cat("\n\n<><><><><><><><> method:", mets[x], "<><><><><><><><><><><><><><><><><><><><>") ## *~*~*~*~*
-              skiptoNext <- FALSE
-              
-              # the error seems to come from levels with zero observations
-              # so maybe ampute() is removing all the camera fates of category X (F in this case)
-              tryCatch(
+                skiptoNext <- FALSE
+                ## *~*~*~*~*
+                # if (params$deb) cat("\n\n<><><><><><><><> method:", mets[x], "\t- ")
+                #if (params$deb) cat("\n\n<><><><><><><><> method:", mets[x], "<><><><><><><><><><><><><><><><><><><><>") ## *~*~*~*~*
+
+                # the error seems to come from levels with zero observations
+                # so maybe ampute() is removing all the camera fates of category X (F in this case)
+                tryCatch(
                 expr = {
-                  # if(debugging){
-                  #   x <- 2
-                  #   
-                  # }
-                  vals <- mkImpSim(aDat=datNA,
+                    vals <- mkImpSim(aDat=datNA,
                                    #cols=col_list,
                                    # pr_list=prVars,
                                    # resp=params$resp, 
@@ -204,6 +213,7 @@ for (seed in params$seeds){
                                    form_list =form_list,
                                    met_list=metLists[,,,mod],
                                    vars=vars,
+                                   outFile=outf,
                                    #mods=mods4sim,
                                    modd=mods4sim[mod],
                                    met=mets[x],
@@ -212,52 +222,37 @@ for (seed in params$seeds){
                                    xdebug=params$xdeb,
                                    impplot=params$ipl
                                    )
-                  # imp <- eval(parse(text=impCall))
-                  # skiptoNext <- FALSE
-                  # if(length(imp$loggedEvents > 0)) print(imp$loggedEvents)
                 },
                 error = function(e){
-                  cat("\nERROR:", conditionMessage(e), "\n")
-                  # next
-                  # return(NULL)
-                  # skiptoNext <- TRUE
-                  skiptoNext <<- TRUE  # superassignment operator- not sure if necessary
-                  # cat(levels)
-                  # imp <- list(imp=NA)
-                  # ret[,,y]
-                  # continue()
+                    cat("\nERROR:", conditionMessage(e), "\n")
+                    skiptoNext <<- TRUE  # superassignment operator- not sure if necessary
                 }
-              )
-            if(skiptoNext) next
+                )
+                if(skiptoNext) next
+                res[, mets[x], run,,mod,]  <- vals
+            }
             
-          #  vmatch <- match(rownames(vals), rownames(res)) # col 1 of vals is the row names
-            #print(head(res))
-            # print(res[,mets[x],run,,mod])
-            # print(vals)
-            #res[vmatch, mets[x], run,,]  <- vals
-            res[, mets[x], run,,mod,]  <- vals
-            #if(params$deb) cat(sprintf("\n\n::::::::::::::::::::::::::::::: ALL OUTPUT - RUN %s :::::::::::::::::::::::::::::::\n\n",run ))## *~*~*~*~*
-            #if(params$deb) print(res[,, run,,,])
-              # res[,mets[x],run,,mod,]
-              # vals
-              # res
-          }
-            
+        if(params$test) cat(sprintf("\n\n::::::::::::::::::::::::::::::: ALL OUTPUT - RUN %s, MODEL %s:::::::::::::::::::::::::::::::\n\n",run,mod ))## *~*~*~*~*
+        if(params$test) qvcalc::indentPrint(res[,, run,,mod,])
             if(run %% params$j == 0){
                     begn <- run-params$j
                     endd <- run-0
                     #cat("\n>>> creating directory:", now_dir, "exists?", exists(now_dir))
                     nowtime <- format(Sys.time(), "%H_%M")
                     # fname <- paste0(now_dir,sprintf("runs%sto%s_%s_seed%s_%s.rds", begn, endd, params$resp, seed, nowtime))
-                    fname <- paste0(now_dir,sprintf("/runs%sto%s_seed%s_%s.rds", begn, endd, seed, nowtime))
+                    fname <- paste0(now_dir,sprintf("/runs%sto%s_mod%s_seed%s_%s_%s.rds", begn, endd,mod, seed, nowtime, suffix))
                     saveRDS(res[,,begn:endd,,,], fname)
                     cat(sprintf("\n>>>>>> %s - saved runs %s to %s to file: %s\n\n",nowtime, begn, endd, fname))
             }
-      }
+        }
 	if (seed_out){
-		writeLines(as.character(seed), "seed.flag")
-		cat("\n>> wrote seed value to file\n")
+            writeLines(as.character(seed), "seed.flag")
+            cat("\n>> wrote seed value to file\n")
 	}
+        nowtime <- format(Sys.time(), "%H_%M")
+        fname <- paste0(now_dir, sprintf("/vals_mod%s_seed%s_%s_%s.rds", mod, seed, nowtime, suffix))
+        cat("\n ~ ~ ~ saving to file:", fname)
+        saveRDS(res, fname)
     }
 }
 
